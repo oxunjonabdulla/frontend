6c32e93470cbf1a584b7a7b6ddf338e03705828e
Asrbek <87761511+AsrbekCoder@users.noreply.github.com>
2024-02-14 12:23:57 +0500

    first commit

--- src/utils/PrivateComponent.jsx
+++ src/utils/PrivateComponent.jsx
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { isAuth } from "../Service/authService";
 
 export const ProtectedRoute = ({ redirectPath = "/", element }) => {
-  const navigate = useNavigate();
   const location = useLocation();
   const auth = isAuth();
   if (!auth) {
-    navigate(redirectPath, { state: { from: location }, replace: true });
+    return <Navigate to={redirectPath} state={{ from: location }} replace />;
   }
 
   return element;
