eda6ca90d062dd0ae57a104f5e14d8381c6be0e5
javohir <javoxir8177@gmail.com>
2024-10-14 10:30:00 +0500

    VU 31 in VU 10 column deleted

--- src/pages/CurrentRepairPage/VU_31/components/VU_31_Table.jsx
+++ src/pages/CurrentRepairPage/VU_31/components/VU_31_Table.jsx
@@ -154,9 +154,7 @@ const VU_31_Table = memo(function VU_31_Table({ gettingData, currentPage }) {
               <Td whiteSpace={"nowrap"}>{item.tamir_yulida_gr13} soat</Td>
               <Td whiteSpace={"nowrap"}>{item.tamir_vaqtida_gr13} soat</Td>
               <Td whiteSpace={"nowrap"}>{item.umumiy_turish_gr11} soat</Td>
-              <Td>
-                <Button colorScheme="blue">VU-10</Button>
-              </Td>
+            
               <Td>
                 <Flex gap={2} m={0}>
                   <Button
