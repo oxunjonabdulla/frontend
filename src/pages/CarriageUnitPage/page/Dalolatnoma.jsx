f056f80031e96a5cf46b8955ef199d49c27cf479
oxunjonabdulla <coderoxunjon50@gmail.com>
2026-02-11 17:23:48 +0500

    Aravalar act is being fixed

--- src/pages/CarriageUnitPage/page/Dalolatnoma.jsx
+++ src/pages/CarriageUnitPage/page/Dalolatnoma.jsx
@@ -17,280 +17,265 @@ import {
   Thead,
   Tr,
   useDisclosure,
+  Badge,
 } from "@chakra-ui/react";
-import { faBook, faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
-import { SliderMock } from "@/utils";
-import { carriage_dalolatnoma_head } from "@/utils/mock_heads";
-import { Auto_dalolatnoma_model } from "./modal/AutoDalolatnoma/Auto_dalolatnoma_model";
-import UserApi from "@/Service/module/userModule.api";
 
-import { faEye } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faPlus, faTrashAlt, faEye } from "@fortawesome/free-solid-svg-icons";
+
+import { useEffect, useState } from "react";
+import UserApi from "@/Service/module/userModule.api";
+import { useDebounce } from "../../../hooks/useDebounce";
+
+import { Pagination, IsImzo, Deleteted } from "../../../components";
+import { Dalolatnoma_Show } from "./Dalolatnoma_show";
 import { Orqa } from "./modal/AutoDalolatnoma/Orqa";
 import { ShowBack } from "./modal/AutoDalolatnoma/ShowBack";
-import { Deleteted, ImageSignature, IsImzo, Pagination } from "../../../components";
-import { repairTypesName } from "../../../utils";
-import { useDebounce } from "../../../hooks/useDebounce";
-import { Dalolatnoma_Show } from "./Dalolatnoma_show";
+const STATUS_MAP = {
+  draft: { text: "Boshlanmagan", color: "gray" },
+  collect_done: { text: "Yig‘uv tugadi", color: "blue" },
+  in_progress: { text: "Jarayonda", color: "orange" },
+  completed: { text: "Yakunlandi", color: "green" },
+  archived: { text: "Arxiv", color: "purple" },
+};
 
 export const CarriageDalolatnoma = () => {
-  const { isOpen, onClose, onOpen } = useDisclosure();
-  const {
-    isOpen: isOPenBack,
-    onClose: onCloseBack,
-    onOpen: onOpenBack,
-  } = useDisclosure();
-  const {
-    isOpen: isOpenShowBack,
-    onClose: onCloseShowBack,
-    onOpen: onOpenShowBack,
-  } = useDisclosure();
+  // ===== MODALS =====
+  const actPreview = useDisclosure();
+  const aravaCreate = useDisclosure();
+  const aravaShow = useDisclosure();
+const StatusBadge = ({ status }) => {
+  const conf = STATUS_MAP[status] || STATUS_MAP.draft;
 
-  const [isLoadingData, setIsLoading] = useState(true);
-
-  const [currentPage, setCurrentPage] = useState(0);
-  const [gettingData, setGettingData] = useState([]);
-  const [backId, setBackId] = useState(null);
-  const [deleteID, setDeleteID] = useState(null);
-  const [deleteModel, setDeleteModal] = useState(false);
-  const [showBack, setShowBackData] = useState(0);
-  const [showAllData, setShowAllData] = useState(null);
-  const [searchValue, setSearchValue] = useState(null);
-  const carriageSerach = useDebounce(searchValue);
-  const {
-    isOpen: isOpenShowAll,
-    onClose: onCloseShowAll,
-    onOpen: onOpenShowAll,
-  } = useDisclosure();
-
-  const handlePageClick = (data) => {
-    const selectedPage = data.selected;
-    setCurrentPage(selectedPage);
+  const GRADIENTS = {
+    gray: "linear(to-r, gray.400, gray.600)",
+    blue: "linear(to-r, blue.400, blue.600)",
+    orange: "linear(to-r, orange.400, orange.600)",
+    green: "linear(to-r, green.400, green.600)",
+    purple: "linear(to-r, purple.400, purple.600)",
   };
-  const handleBack = (data) => {
-    onOpenBack();
-    setBackId(data);
-  };
-  const handleShowBack = (data) => {
-    onOpenShowBack();
-    setShowBackData(data);
-  };
-  const handleDelate = async (carriageID) => {
-    const { response } = await new UserApi().deleteAravaAct(carriageID);
-    if (response) {
-      window.location.reload();
-    }
-  };
-  const handleShowAll = (data) => {
-    onOpenShowAll();
-    setShowAllData(data);
-  };
-  useEffect(() => {
-    const fetchData = async () => {
-      const paramsPage = {
-        page: currentPage + 1,
-
-        ...(carriageSerach && { search: carriageSerach }),
-      };
-      setIsLoading(true);
-      const { response } = await new UserApi().getALlAravaAct(paramsPage);
-      if (response) {
-        setIsLoading(false);
-        setGettingData(response?.data);
-      }
-    };
-    fetchData();
-  }, [carriageSerach, currentPage]);
 
   return (
-    <Box
-      as="div"
-      bg={"#ffff"}
-      my={8}
-      mx="auto"
-      rounded={"lg"}
-      position={"relative"}
+    <Badge
+      px={3}
+      py={1}
+      rounded="full"
+      bgGradient={GRADIENTS[conf.color]}
+      color="white"
+      fontSize="0.7rem"
+      fontWeight="600"
+      boxShadow="sm"
+      textTransform="uppercase"
     >
-      <Heading as={"h3"} size={"lg"} mb={5} textAlign={"center"}>
-          Kirish va chiqish dalolatnomasi
+      {conf.text}
+    </Badge>
+  );
+};
+  // ===== STATE =====
+  const [loading, setLoading] = useState(true);
+  const [acts, setActs] = useState({ count: 0, results: [] });
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const [selectedAct, setSelectedAct] = useState(null);
+  const [selectedArava, setSelectedArava] = useState(null);
+
+  const [deleteID, setDeleteID] = useState(null);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
+
+  // ===== LOAD ACTS =====
+  useEffect(() => {
+    const loadActs = async () => {
+      setLoading(true);
+
+      const params = {
+        page: currentPage + 1,
+        department: "arava",
+        ...(debouncedSearch && { search: debouncedSearch }),
+      };
+
+      const { response } = await new UserApi().getActs(params);
+      if (response) setActs(response.data);
+
+      setLoading(false);
+    };
+
+    loadActs();
+  }, [currentPage, debouncedSearch]);
+
+  // ===== HANDLERS =====
+  const openAct = (act) => {
+    setSelectedAct(act);
+    actPreview.onOpen();
+  };
+
+  const addArava = (act) => {
+    setSelectedAct(act);
+    aravaCreate.onOpen();
+  };
+
+  const showArava = (arava) => {
+    setSelectedArava(arava);
+    aravaShow.onOpen();
+  };
+
+  // ===== UI =====
+  return (
+    <Box bg="white" p={6} rounded="lg" boxShadow="sm">
+
+      <Heading textAlign="center" mb={6}>
+        Aravalar bo‘limi (Kirish-chiqish dalolatnomasi)
       </Heading>
-      <Button
-        borderRadius={"50%"}
-        colorScheme="teal"
-        width={"50px"}
-        height={"50px"}
-        position={"absolute"}
-        right={3}
-        top={-12}
-        onClick={onOpen}
-      >
-        +
-      </Button>
-      <Box my={3}>
-        <FormControl w={"250px"}>
-          <FormLabel>Vagon raqam bo&apos;yicha qidirish</FormLabel>
-          <Input
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Vagon Raqami Yozing"
-            borderColor={"gray.600"}
-            type="text"
-          />
-        </FormControl>
-      </Box>
-      {!isLoadingData ? (
-        gettingData?.results?.length ? (
-          <TableContainer p={4} border={"1px solid #eeeee"}>
-            <Table
-              borderRadius={10}
-              size={"sm"}
-              whiteSpace={"pre-wrap"}
-              variant={"striped"}
-              overflow={"hidden"}
-              colorScheme="blackAlpha"
-              shadow={"lg"}
-            >
-              <Thead bg={"#0c6170"} rounded={10}>
-                <Tr>
-                  {carriage_dalolatnoma_head?.map((item) => (
-                    <Th
-                      textAlign={"center"}
-                      key={item.label}
-                      colSpan={item.colspan}
-                    >
-                      {item.label}
-                    </Th>
-                  ))}
-                </Tr>
-              </Thead>
 
-              <Tbody>
-                {gettingData?.results?.map((item, idx) => (
-                  <Tr key={item.carriage}>
-                    <Td>{currentPage * 10 + idx + 1}</Td>
-                    <Td fontWeight={700}>{item?.carriage}</Td>
-                    <Td>{repairTypesName(item?.front_detail.repair_type)}</Td>
-                    <Td>{item?.front_detail.yon_raqam_1}</Td>
-                    <Td>{item?.front_detail.yon_raqam_2}</Td>
-                    <Td>{item?.front_detail.yon_raqam_3}</Td>
-                    <Td>{item?.front_detail.yon_raqam_4}</Td>
-                    <Td>{item?.front_detail.auto_zavod_1}</Td>
-                    <Td>{item?.front_detail.auto_zavod_2}</Td>
-                    <Td>{item?.front_detail.auto_zavod_3}</Td>
-                    <Td>{item?.front_detail.auto_zavod_4}</Td>
-                    <Td>{item?.front_detail.mavjud_kod_1}</Td>
-                    <Td>{item?.front_detail.mavjud_kod_2}</Td>
-                    <Td>{item?.front_detail.mavjud_kod_3}</Td>
-                    <Td>{item?.front_detail.mavjud_kod_4}</Td>
-                    <Td>
-                      Yon №1: {item?.front_detail.restor_balka_yon_1}
-                      <Divider my={2} />
-                      Yon №2: {item?.front_detail.restor_balka_yon_2}
-                    </Td>
-                    <Td>
-                      Zavod Tamgasi №1:{" "}
-                      {item?.front_detail.restor_balka_zavod_1}
-                      <Divider my={2} />
-                      Zavod Tamgasi №2:{" "}
-                      {item?.front_detail.restor_balka_zavod_2}
-                    </Td>
-                    <Td>
-                      Mavjudlik Kodi №1: {item?.front_detail.restor_balka_kod_1}
-                      <Divider my={2} />
-                      Mavjudlik Kodi №2: {item?.front_detail.restor_balka_kod_2}
-                    </Td>
-                    <Td>
-                      <IsImzo isImzo={item?.avto_connector_plumber_signature_user} />
-                    </Td>
-                    <Td>
-                      <IsImzo isImzo={item?.avto_connector_defektospopistr_signature_user} />
-                    </Td>
-                    <Td color={"teal"}>
-                      {!item?.back_detail ? (
-                        <Flex justify={"center"} align={"center"} gap={2} m={0}>
-                          <Text>Qo'shish</Text>
+      {/* SEARCH */}
+      <FormControl maxW="300px" mb={4}>
+        <FormLabel>Vagon raqam bo‘yicha qidirish</FormLabel>
+        <Input
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            setCurrentPage(0);
+          }}
+          placeholder="Vagon raqamini yozing"
+        />
+      </FormControl>
 
-                            <IconButton
-                              onClick={() => handleBack(item?.carriage)}
-                              bg="blue.500"
-                              color="white"
-                              icon={<FontAwesomeIcon icon={faPlus} />}
-                            />
+      {/* TABLE */}
+      {loading ? (
+        <Flex justify="center" my={10}>Yuklanmoqda...</Flex>
+      ) : acts.results.length ? (
 
-                        </Flex>
-                      ) : (
-                        <Flex justify={"center"} gap={2} m={0}>
-                          <IconButton
-                            bg="blue.500"
-                              color="white"
-                            onClick={() => handleShowBack(item?.back_detail)}
-                            icon={<FontAwesomeIcon icon={faEye} />}
-                          />
+        <TableContainer border="1px solid #eee" rounded="lg">
+          <Table size="sm" variant="striped">
 
-                        </Flex>
-                      )}
-                    </Td>
-                    <Td>
-                      <Flex gap={2} m={0}>
+            <Thead bg="#0c6170">
+              <Tr>
+                <Th color="white">T/r</Th>
+                <Th color="white">Vagon</Th>
+                <Th color="white">Dalolatnoma holati</Th>
+                <Th color="white">Arava holati</Th>
+                <Th color="white">Imzolar</Th>
+                <Th color="white">Amallar</Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              {acts.results.map((act, idx) => (
+                <Tr key={act.id}>
+
+                  <Td>{currentPage * 10 + idx + 1}</Td>
+                  <Td fontWeight="600">{act.carriage_number}</Td>
+
+
+
+                   <Td textAlign="center">
+                        <StatusBadge status={act.status} />
+                     </Td>
+
+
+                  <Td>
+                    {act.arava_act ? (
+                      <Badge colorScheme="red">To‘ldirilgamagan</Badge>
+                    ) : (
+                      <Badge colorScheme="red">Yo‘q</Badge>
+                    )}
+                  </Td>
+
+                  <Td>
+                    <IsImzo isImzo={act.arava_act?.repair_master_signature} />
+                  </Td>
+
+                  <Td>
+                    <Flex gap={2} justify="center">
+
+                      {/* VIEW ACT */}
+                      <IconButton
+                        size="sm"
+                        colorScheme="teal"
+                            icon={<FontAwesomeIcon icon={faPlus} />}
+                        onClick={() => openAct(act)}
+                      />
+
+                      {/* ADD / VIEW ARAVA */}
+                      {!act.arava_act ? (
                         <IconButton
-                          colorScheme="red"
-                          onClick={() => {
-                            setDeleteID(item?.carriage);
-                            setDeleteModal(true);
-                          }}
-                          icon={<FontAwesomeIcon icon={faTrashAlt} />}
+                          size="sm"
+                          colorScheme="blue"
+                          icon={<FontAwesomeIcon icon={faPlus} />}
+                          onClick={() => addArava(act)}
                         />
-                      </Flex>
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        ) : (
-          <Flex align={"center"} flexDir={"column"} my={12} gap={4}>
-            <FontAwesomeIcon icon={faBook} fontSize={"70px"} opacity={"0.4"} />
-            <Text
-              as={"h1"}
-              fontWeight={600}
-              textAlign={"center"}
-              fontSize={"2xl"}
-            >
-             Kirish va chiqish nazorati dalolatnomasi topilmadi
-            </Text>
-            <Button colorScheme="teal" onClick={onOpen}>
-              Qo‘shish
-            </Button>
-          </Flex>
-        )
+                      ) : (
+                        <IconButton
+                          size="sm"
+                          colorScheme="blue"
+                          icon={<FontAwesomeIcon icon={faEye} />}
+                          onClick={() => showArava(act.arava_act)}
+                        />
+                      )}
+
+                      {/* DELETE */}
+                      <IconButton
+                        size="sm"
+                        colorScheme="red"
+                        icon={<FontAwesomeIcon icon={faTrashAlt} />}
+                        onClick={() => {
+                          setDeleteID(act.id);
+                          setDeleteOpen(true);
+                        }}
+                      />
+                    </Flex>
+                  </Td>
+
+                </Tr>
+              ))}
+            </Tbody>
+
+          </Table>
+        </TableContainer>
+
       ) : (
-        <SliderMock setIsLoading={setIsLoading} />
+        <Flex direction="column" align="center" my={12}>
+          <FontAwesomeIcon icon={faBook} size="3x" opacity={0.4} />
+          <Text fontSize="xl" mt={3}>ACT topilmadi</Text>
+        </Flex>
       )}
 
-      {gettingData?.results?.length ? (
-        <Pagination
-          pageCount={gettingData?.count}
-          onPageChange={handlePageClick}
-        />
-      ) : null}
+      {/* PAGINATION */}
+      <Pagination
+        pageCount={acts.count}
+        onPageChange={(d) => setCurrentPage(d.selected)}
+      />
+
+      {/* MODALS */}
       <Dalolatnoma_Show
-        isOpen={isOpenShowAll}
-        onClose={onCloseShowAll}
-        data={showAllData}
+        isOpen={actPreview.isOpen}
+        onClose={actPreview.onClose}
+        data={selectedAct}
       />
-      <Orqa isOpen={isOPenBack} onClose={onCloseBack} carriageID={backId} />
+
+      <Orqa
+        isOpen={aravaCreate.isOpen}
+        onClose={aravaCreate.onClose}
+        act={selectedAct}
+      />
+
       <ShowBack
-        isOpen={isOpenShowBack}
-        onClose={onCloseShowBack}
-        dataBack={showBack}
+        isOpen={aravaShow.isOpen}
+        onClose={aravaShow.onClose}
+        dataBack={selectedArava}
       />
+
       <Deleteted
-        isOpen={deleteModel}
-        onClose={setDeleteModal}
+        isOpen={deleteOpen}
+        onClose={setDeleteOpen}
         carriageNumber={deleteID}
-        deletedFunction={handleDelate}
       />
-      <Auto_dalolatnoma_model isOpen={isOpen} onClose={onClose} />
+
     </Box>
   );
 };
+
+export default CarriageDalolatnoma;
